3c98d783cc38baad4823c78f1defd0bb74848dfe
송지은 <songje1005@naver.com>
2023-03-16 21:15:53 +0900

    3주차 예외처리 수정

--- 2주차/Enter_Roman_numerals/Enter_Roman_numerals/main.cpp
+++ 2주차/Enter_Roman_numerals/Enter_Roman_numerals/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+#include <thread>
 #include <conio.h>
+#include <Windows.h>
 
 using namespace std;
 
@@ -21,12 +23,18 @@ int romanToArabic(char c) {
 string romanNum;
 int arabicNum = 0;
 
+void inputThread() {
+    while (true) {
+        if (GetAsyncKeyState(VK_ESCAPE)) {
+            exit(0);
+        }
+    }
+}
+
 int Render() {
 
     while (true) {
         cout << "로마자 숫자를 입력하세요 (종료하려면 esc 키를 누르세요): ";
-
-        //romanNum = _getch(); //입력 받기
 
         romanNum = _getch();
         if (romanNum[0] != 27)
@@ -36,35 +44,57 @@ int Render() {
         }
 
         if (romanNum.empty()) { // 입력 오류 처리
-            cout << "입력 오류입니다. 프로그램을 종료합니다." << endl;
+            cout << "입력 오류입니다. 프로그램을 종료합니다.";
             return 1;
         }
 
         // 로마자 숫자를 아라비아 숫자로 변환하는 과정
         arabicNum = 0;
         for (int i = 0; i < romanNum.length(); i++) {
-            if (romanToArabic(romanNum[i]) == 0 || romanToArabic(romanNum[i]) < 4000) { // 유효하지 않은 문자 처리
-                cout << "잘못된 입력입니다. 다시 입력해주세요." << endl;
+            // 유효하지 않은 문자 처리
+            if (romanToArabic(romanNum[i]) == 0) { // 로마자 문자가 아니면
+                cout << "잘못된 입력입니다. 다시 입력해주세요.";
                 break;
             }
-
+            // 같은 문자가 네 번 이상 반복되는 경우 처리
+            if (i < romanNum.length() - 3 && romanNum[i] == romanNum[i + 1] && romanNum[i + 1] == romanNum[i + 2] && romanNum[i + 2] == romanNum[i + 3]) {
+                cout << "입력값 범위를 넘었습니다. 다시 입력해주세요.";
+                break;
+            }
+            // 같은 문자가 두 번 연속으로 나오는 경우 처리
+            if (i < romanNum.length() - 1 && romanNum[i] == romanNum[i + 1] && (romanNum[i] == 'V' || romanNum[i] == 'L' || romanNum[i] == 'D')) {
+                cout << "잘못된 입력입니다. 다시 입력해주세요.";
+                break;
+            }
+            // 규칙에 어긋나는 조합의 경우 처리
             if (i < romanNum.length() - 1 && romanToArabic(romanNum[i]) < romanToArabic(romanNum[i + 1])) {
-                arabicNum -= romanToArabic(romanNum[i]); // 작은 수가 큰 수 앞에 오는 경우
+                if (romanNum[i] == 'I' && (romanNum[i + 1] == 'V' || romanNum[i + 1] == 'X')) {
+                    arabicNum -= romanToArabic(romanNum[i]);
+                }
+                else if (romanNum[i] == 'X' && (romanNum[i + 1] == 'L' || romanNum[i + 1] == 'C')) {
+                    arabicNum -= romanToArabic(romanNum[i]);
+                }
+                else if (romanNum[i] == 'C' && (romanNum[i + 1] == 'D' || romanNum[i + 1] == 'M')) {
+                    arabicNum -= romanToArabic(romanNum[i]);
+                }
+                else {
+                    cout << "잘못된 입력입니다.";
+                }
             }
             else {
                 arabicNum += romanToArabic(romanNum[i]);
             }
         }
         cout << "아라비아 숫자로 변환한 결과: " << arabicNum << endl;
-        //if (romanNum.length() == arabicNum) { // 유효한 입력 처리
-        //    cout << "아라비아 숫자로 변환한 결과: " << arabicNum << endl;
-        //}
     }
 }
 
 int main() {
 
+    thread inputThreadObj(inputThread); // 스레드 생성
+
     Render();
 
+    inputThreadObj.join(); // 스레드 종료 대기
     return 0;
 }
