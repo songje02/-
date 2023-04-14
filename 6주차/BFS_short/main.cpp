2aa290c68f006d515df7c6d7633e88b2881ef7bf
송지은 <songje1005@naver.com>
2023-04-15 08:46:22 +0900

    문제해결능력_중간고사_최단거리

--- 6주차/BFS_short/main.cpp
+++ 6주차/BFS_short/main.cpp
@@ -42,8 +42,24 @@ int bfs(int sx, int sy, int ex, int ey) {
             q.push({ nx, ny, cur.d + 1 }); // 큐에 삽입
         }
     }
-
     return -1; // 도착하지 못한 경우
+}
+
+void printPath(int curX, int curY) {
+    for (int i = 0; i < 4; i++) { // 이동 가능한 모든 경우를 고려
+        int nx = curX + dx[i];
+        int ny = curY + dy[i];
+
+        if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue; // 범위를 벗어나는 경우
+        if (dist[nx][ny] == -1) continue; // 갈 수 없는 곳
+        if (dist[nx][ny] < dist[curX][curY]) { // 이동한 경로일 경우
+            curX = nx;
+            curY = ny;
+            break;
+        }
+    }
+    cout << "====================" << endl;
+
 }
 
 void printMap(int playerX, int playerY, int enemyX, int enemyY, const vector<vector<int>>& map) {
@@ -90,8 +106,10 @@ void printMap(int playerX, int playerY, int enemyX, int enemyY, const vector<vec
         }
         cout << "====================" << endl;
 
+        printPath(curX, curY);
     }
 }
+
 
 void Input() {
     cout << "모든 입력은 띄어쓰기로 구별합니다." << endl;
