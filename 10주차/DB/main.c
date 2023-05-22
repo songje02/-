130407e67d56e40e85c431f023bc76d57afd7d45
송지은 <songje1005@naver.com>
2023-05-22 17:52:54 +0900

    [10주차] DB 수정-미완

--- 10주차/DB/main.c
+++ 10주차/DB/main.c
@@ -1,64 +1,64 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//
-//void saveValues(const char* str1, const char* str2, int* intValue, char* charValue, char* strValue) {
-//    char* token = strtok(str2, " ");
-//
-//    // 정수
-//    char* intToken = strstr(str1, "%d");
-//    if (intToken != NULL) {
-//        *intValue = atoi(token);
-//        token = strtok(NULL, " ");
-//    }
-//
-//    // 문자
-//    char* charToken = strstr(str1, "%c");
-//    if (charToken != NULL) {
-//        *charValue = token[0];
-//        token = strtok(NULL, " ");
-//    }
-//
-//    // 문자열
-//    char* strToken = strstr(str1, "%s");
-//    if (strToken != NULL) {
-//        strcpy(strValue, token);
-//        token = strtok(NULL, " ");
-//    }
-//}
-//
-//int main() {
-//    FILE* file = fopen("db.txt", "r");
-//    if (file == NULL) {
-//        printf("파일을 읽으 수 없습니다.");
-//        return 1;
-//    }
-//
-//    char str1[100];
-//    char str2[100];
-//    int* intValue;
-//    char* charValue;
-//    char* strValue[100];
-//
-//    while (fgets(str1, sizeof(str1), file) && fgets(str2, sizeof(str2), file)) {
-//        if (str1[strlen(str1) - 1] == '\n') {
-//            str1[strlen(str1) - 1] = '\0';  
-//        }
-//
-//        if (str2[strlen(str2) - 1] == '\n') {
-//            str2[strlen(str2) - 1] = '\0'; 
-//        }
-//
-//        saveValues(str1, str2, &intValue, &charValue, strValue);
-//
-//        printf("정수: %d\n", intValue);
-//        printf("문자: %c\n", charValue);
-//        printf("문자열: %s\n", strValue);
-//        printf("\n");
-//    }
-//
-//    fclose(file);
-//
-//    return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void saveValues(const char* str1, const char* str2, int* intValue, char* charValue, char* strValue) {
+    char* token = strtok(str2, " ");
+
+    // 정수
+    char* intToken = strstr(str1, "%d");
+    if (intToken != NULL) {
+        *intValue = atoi(token);
+        token = strtok(NULL, " ");
+    }
+
+    // 문자
+    char* charToken = strstr(str1, "%c");
+    if (charToken != NULL) {
+        *charValue = token[0];
+        token = strtok(NULL, " ");
+    }
+
+    // 문자열
+    char* strToken = strstr(str1, "%s");
+    if (strToken != NULL) {
+        strcpy(strValue, token);
+        token = strtok(NULL, " ");
+    }
+}
+
+int main() {
+    FILE* file = fopen("db.txt", "r");
+    if (file == NULL) {
+        printf("파일을 읽으 수 없습니다.");
+        return 1;
+    }
+
+    char str1[100];
+    char str2[100];
+    int* intValue;
+    char* charValue;
+    char* strValue[100];
+
+    while (fgets(str1, sizeof(str1), file) && fgets(str2, sizeof(str2), file)) {
+        if (str1[strlen(str1) - 1] == '\n') {
+            str1[strlen(str1) - 1] = '\0';  
+        }
+
+        if (str2[strlen(str2) - 1] == '\n') {
+            str2[strlen(str2) - 1] = '\0'; 
+        }
+
+        saveValues(str1, str2, &intValue, &charValue, strValue);
+
+        printf("정수: %d\n", intValue);
+        printf("문자: %c\n", charValue);
+        printf("문자열: %s\n", strValue);
+        printf("\n");
+    }
+
+    fclose(file);
+
+    return 0;
+}
--- 10주차/DB/main4.c
+++ 10주차/DB/main4.c
@@ -4,14 +4,7 @@
 //#include <string.h>
 //
 //void tokenValues(char* str1, char* str2, int* intValue, char* charValue, char* strValue) {
-//    char* Token = strtok(str1, "%");
-//
-//    while (Token != NULL) {
-//        strcpy(strValue, Token);
-//        Token = strtok(NULL, "%");
-//    }
-//
-//    char* strArr[100] = { NULL, };
+//    char* strArr[100] = { NULL };
 //    char* strToken = strtok(str2, " ");
 //    int i = 0;
 //    while (strToken != NULL) {
@@ -20,37 +13,31 @@
 //        strToken = strtok(NULL, " ");
 //    }
 //
-//     정수
+//    // Find and store integer value
 //    char* intToken = strstr(str1, "%d");
 //    if (intToken != NULL) {
-//        *intValue = atoi(strArr[5]);
-//    }
-//
-//     문자
-//    char* charToken = strstr(str1, "%c");
-//    if (charToken != NULL) {
-//        *charValue = strArr[5][0];
-//    }
-//
-//     문자열 길이
-//    char* strToken2 = strstr(str1, "%s");
-//    if (strToken2 != NULL) {
-//        strcpy(strValue, strArr[5]);
-//        *intValue = strlen(strArr[5]);
-//    }
-//
-//     str2에서 일치하는 값 제거
-//    for (int j = 0; strArr[j] != NULL; j++) {
-//        if (strcmp(strArr[j], strValue) == 0) {
-//            strcpy(strArr[j], "");
+//        int index = atoi(intToken + 2) - 1; // Adjust index by subtracting 1
+//        if (index < i) {
+//            *intValue = atoi(strArr[index]);
 //        }
 //    }
 //
-//     남은 값 추출
-//    for (int j = 0, k = 0; strArr[j] != NULL; j++) {
-//        if (strlen(strArr[j]) > 0) {
-//            strcpy(strValue, strArr[j]);
-//            k++;
+//    // Find and store character value
+//    char* charToken = strstr(str1, "%c");
+//    if (charToken != NULL) {
+//        int index = atoi(charToken + 2) - 1; // Adjust index by subtracting 1
+//        if (index < i) {
+//            *charValue = strArr[index];
+//        }
+//    }
+//
+//    // Find and store string value
+//    char* strToken2 = strstr(str1, "%s");
+//    if (strToken2 != NULL) {
+//        int index = atoi(strToken2 + 2) - 1; // Adjust index by subtracting 1
+//        if (index < i) {
+//            strcpy(strValue, strArr[index]);
+//            *intValue = strlen(strArr[index]);
 //        }
 //    }
 //}
