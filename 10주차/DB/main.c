5f2dd80b2134790dc6400b50d9efa62e2d73b6cd
송지은 <songje1005@naver.com>
2023-05-24 00:47:57 +0900

    ]
    void saveValues(const char* str1, const char* str2, char* strVal, int* intVal, char* charVal) {
        char* tokens1[100];
        char* tokens2[100];
        int tokenCount1 = 0;
        int tokenCount2 = 0;
    
        char* token = strtok(str1, " ");
        while (token != NULL) {
            tokens1[tokenCount1++] = token;
            token = strtok(NULL, " ");
        }
    
        token = strtok(str2, " ");
        while (token != NULL) {
            tokens2[tokenCount2++] = token;
            token = strtok(NULL, " ");
        }
        for (int i = 0; i < tokenCount1; i++) {
            if (strcmp(tokens1[i], tokens2[i]) != 0) {
                // 문자열
                if (strstr(tokens1[i], "%s") != NULL) {
                    strcpy(strVal, tokens2[i]);
                }
                // 정수
                else if (strstr(tokens1[i], "%d") != NULL) {
                    *intVal = atoi(tokens2[i + 1]) / 10000;
                }
                // 문자
                else if (strstr(tokens1[i], "%c") != NULL) {
                    *charVal = tokens2[i + 1][0];
                }
            }
        }
    }
    
    int main() {
        FILE* file = fopen("db.txt", "r");
        if (file == NULL) {
            printf("파일을 읽을 수 없습니다.");
            return 1;
        }
    
        char str1[100];
        char str2[100];
        char strVal[100];
        int intVal;
        char charVal;
    
        while (fgets(str1, sizeof(str1), file) && fgets(str2, sizeof(str2), file)) {
            if (str1[strlen(str1) - 1] == '\n') {
                str1[strlen(str1) - 1] = '\0';
            }
    
            if (str2[strlen(str2) - 1] == '\n') {
                str2[strlen(str2) - 1] = '\0';
            }
    
            saveValues(str1, str2, strVal, &intVal, &charVal);
    
            printf("이름: %s\n", strVal);
            printf("점수: %d\n", intVal);
            printf("성적: %c\n", charVal);
            printf("\n");
        }
    
        fclose(file);
    
        return 0;
    }

--- 10주차/DB/main.c
+++ 10주차/DB/main.c
@@ -3,58 +3,68 @@
 #include <stdlib.h>
 #include <string.h>
 
-void saveValues(const char* str1, const char* str2, int* intValue, char* charValue, char* strValue) {
-    char* token = strtok(str2, " ");
+void saveValues(const char* str1, const char* str2, char* strVal, int* intVal, char* charVal) {
+    char* tokens1[100];
+    char* tokens2[100];
+    int tokenCount1 = 0;
+    int tokenCount2 = 0;
 
-    // 정수
-    char* intToken = strstr(str1, "%d");
-    if (intToken != NULL) {
-        *intValue = atoi(token);
+    char* token = strtok(str1, " ");
+    while (token != NULL) {
+        tokens1[tokenCount1++] = token;
         token = strtok(NULL, " ");
     }
 
-    // 문자
-    char* charToken = strstr(str1, "%c");
-    if (charToken != NULL) {
-        *charValue = token[0];
+    token = strtok(str2, " ");
+    while (token != NULL) {
+        tokens2[tokenCount2++] = token;
         token = strtok(NULL, " ");
     }
-
-    // 문자열
-    char* strToken = strstr(str1, "%s");
-    if (strToken != NULL) {
-        strcpy(strValue, token);
-        token = strtok(NULL, " ");
+    for (int i = 0; i < tokenCount1; i++) {
+        if (strcmp(tokens1[i], tokens2[i]) != 0) {
+            // 문자열
+            if (strstr(tokens1[i], "%s") != NULL) {
+                strcpy(strVal, tokens2[i]);
+            }
+            // 정수
+            else if (strstr(tokens1[i], "%d") != NULL) {
+                *intVal = atoi(tokens2[i + 1]) / 10000;
+            }
+            // 문자
+            else if (strstr(tokens1[i], "%c") != NULL) {
+                *charVal = tokens2[i + 1][0];
+            }
+        }
     }
 }
 
 int main() {
     FILE* file = fopen("db.txt", "r");
     if (file == NULL) {
-        printf("파일을 읽으 수 없습니다.");
+        printf("파일을 읽을 수 없습니다.");
         return 1;
     }
 
     char str1[100];
     char str2[100];
-    int* intValue;
-    char* charValue;
-    char* strValue[100];
+    char strVal[100];
+    int intVal;
+    char charVal;
 
     while (fgets(str1, sizeof(str1), file) && fgets(str2, sizeof(str2), file)) {
         if (str1[strlen(str1) - 1] == '\n') {
-            str1[strlen(str1) - 1] = '\0';  
+            str1[strlen(str1) - 1] = '\0';
         }
 
         if (str2[strlen(str2) - 1] == '\n') {
-            str2[strlen(str2) - 1] = '\0'; 
+            str2[strlen(str2) - 1] = '\0';
         }
 
-        saveValues(str1, str2, &intValue, &charValue, strValue);
+        saveValues(str1, str2, strVal, &intVal, &charVal);
 
-        printf("정수: %d\n", intValue);
-        printf("문자: %c\n", charValue);
-        printf("문자열: %s\n", strValue);
+        printf("이름: %s\n", strVal);
+        printf("점수: %d\n", intVal);
+        printf("성적: %c\n", charVal);
         printf("\n");
     }
 
